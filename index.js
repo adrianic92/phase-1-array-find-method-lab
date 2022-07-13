31e4824d5ce50b7fe9cadbe457a52cc2d9c26aa7
Adrian <adrianic92@yahoo.com>
2022-07-12 17:30:36 -0700

    commit tree f27f572f4c4dbcb745bd59666805c37d15aa9407

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+// function superbowlWin(array) {
+//     const year;
+//     return year;
+// }
+
+// const superbowlWin = inventory.find(({record}) => record.result === 'W');
+
+function superbowlWin(record) {
+    const winner = record.find( record => record.result === "W");
+    return !!winner ? winner.year : undefined;
+}
